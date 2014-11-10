7a3389b2ceff55a7adafe3aa689ab7339a63ff22
Suryadev singh yadav <suryadev.singh08@gmail.com>
2014-11-10 23:12:35 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,9 @@
+/**
+ * Copyright (c) 2014 .....
+ * @author        suryadev <suryadev.singh08@gmail.com>
+ * @license       ....
+ * @Description   
+ *
+ */
+
+angular.module("lazyloadingImage", ["lazyloadingImage.controllers","lazyloadingImage.services","infinite-scroll"]);
